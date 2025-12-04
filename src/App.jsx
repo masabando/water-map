c8f9603359f057fe22458bf1bec4cac38a8688c2
Masamitsu Bando <bando@ktc.ac.jp>
2025-12-04 10:56:34 +0900

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,31 @@
-import { useEffect, useReducer, useRef, useState } from 'react'
-import './App.css'
-import NoiseJS from 'noisejs'
-import { init } from '@masabando/easy-three'
+import { useEffect, useReducer, useRef, useState } from "react";
+import "./App.css";
+import NoiseJS from "noisejs";
+import { init } from "@masabando/easy-three";
 
 function App() {
-  const ref = useRef(null)
-  const noiseDetailRef = useRef(null)
-  const noise = useRef(new NoiseJS.Noise(Math.random()))
-  const [noiseDetail, setNoiseDetail] = useState(60)
-  const ctxRef = useRef(null)
-  const [normalMapStrength, setNormalMapStrength] = useState(4.0)
-  const normalMapStrengthRef = useRef(null)
-  const divRef = useRef(null)
-  const easyThreeRef = useRef(null)
-  const [dataURL, setDataURL] = useState("")
-  const noiseDetailTimeout = useRef(null)
-  const normalMapStrengthTimeout = useRef(null)
-  const [stop, setStop] = useState(false)
-  const [updateCount, updateCountDispatch] = useReducer((state => state + 1), 0)
+  const ref = useRef(null);
+  const noiseDetailRef = useRef(null);
+  const noise = useRef(new NoiseJS.Noise(Math.random()));
+  const [noiseDetail, setNoiseDetail] = useState(60);
+  const ctxRef = useRef(null);
+  const [normalMapStrength, setNormalMapStrength] = useState(4.0);
+  const normalMapStrengthRef = useRef(null);
+  const divRef = useRef(null);
+  const easyThreeRef = useRef(null);
+  const [dataURL, setDataURL] = useState("");
+  // const noiseDetailTimeout = useRef(null);
+  // const normalMapStrengthTimeout = useRef(null);
+  const [stop, setStop] = useState(false);
+  const [updateCount, updateCountDispatch] = useReducer(
+    (state) => state + 1,
+    0
+  );
 
   useEffect(() => {
-    const canvas = ref.current
-    ctxRef.current = canvas.getContext('2d')
-  }, [])
+    const canvas = ref.current;
+    ctxRef.current = canvas.getContext("2d");
+  }, []);
 
   // function seamlessNoise2D(x, y, scale, noiseFunc) {
   //   const s = scale
@@ -32,23 +35,24 @@ function App() {
   //   return noiseFunc.perlin3(nx, ny, nz)
   // }
   function seamlessNoise2D(x, y, scale, noiseFunc) {
-    const nx = Math.cos((x * 2 * Math.PI) / scale)
-    const ny = Math.sin((x * 2 * Math.PI) / scale)
-    const nz = Math.cos((y * 2 * Math.PI) / scale)
-    const nt = Math.sin((y * 2 * Math.PI) / scale)
+    const nx = Math.cos((x * 2 * Math.PI) / scale);
+    const ny = Math.sin((x * 2 * Math.PI) / scale);
+    const nz = Math.cos((y * 2 * Math.PI) / scale);
+    const nt = Math.sin((y * 2 * Math.PI) / scale);
     return (
       0.6 * noiseFunc.perlin3(nx, ny, nz) +
       0.4 * noiseFunc.perlin3(nx * 1.8, ny * 0.5, nt * 1.2)
-    )
+    );
   }
 
   useEffect(() => {
-    const canvas = ref.current
-    const ctx = ctxRef.current
+    noise.current = new NoiseJS.Noise(Math.random());
+    const canvas = ref.current;
+    const ctx = ctxRef.current;
 
     // background(255)
-    ctx.fillStyle = '#ffffff'
-    ctx.fillRect(0, 0, canvas.width, canvas.height)
+    ctx.fillStyle = "#ffffff";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
 
     // draw the noise
     // for (let x = 0; x < canvas.width; x++) {
@@ -62,62 +66,61 @@ function App() {
     // }
     for (let x = 0; x < canvas.width; x++) {
       for (let y = 0; y < canvas.height; y++) {
-        const scale = noiseDetail
+        const scale = noiseDetail;
 
-        const hL = seamlessNoise2D(x - 1, y, scale, noise.current)
-        const hR = seamlessNoise2D(x + 1, y, scale, noise.current)
-        const hU = seamlessNoise2D(x, y - 1, scale, noise.current)
-        const hD = seamlessNoise2D(x, y + 1, scale, noise.current)
+        const hL = seamlessNoise2D(x - 1, y, scale, noise.current);
+        const hR = seamlessNoise2D(x + 1, y, scale, noise.current);
+        const hU = seamlessNoise2D(x, y - 1, scale, noise.current);
+        const hD = seamlessNoise2D(x, y + 1, scale, noise.current);
 
-        const dx = (hR - hL) * normalMapStrength
-        const dy = (hD - hU) * normalMapStrength
+        const dx = (hR - hL) * normalMapStrength;
+        const dy = (hD - hU) * normalMapStrength;
 
         // 法線ベクトル (x, y, z)
-        const normal = [-dx, -dy, 1.0]
-        const length = Math.hypot(...normal)
-        const nx = normal[0] / length
-        const ny = normal[1] / length
-        const nz = normal[2] / length
+        const normal = [-dx, -dy, 1.0];
+        const length = Math.hypot(...normal);
+        const nx = normal[0] / length;
+        const ny = normal[1] / length;
+        const nz = normal[2] / length;
 
         // [-1, 1] -> [0, 255] に変換
-        const r = Math.floor((nx * 0.5 + 0.5) * 255)
-        const g = Math.floor((ny * 0.5 + 0.5) * 255)
-        const b = Math.floor((nz * 0.5 + 0.5) * 255)
+        const r = Math.floor((nx * 0.5 + 0.5) * 255);
+        const g = Math.floor((ny * 0.5 + 0.5) * 255);
+        const b = Math.floor((nz * 0.5 + 0.5) * 255);
 
-        ctx.fillStyle = `rgb(${r}, ${g}, ${b})`
-        ctx.fillRect(x, y, 1, 1)
-
+        ctx.fillStyle = `rgb(${r}, ${g}, ${b})`;
+        ctx.fillRect(x, y, 1, 1);
       }
-      setDataURL(canvas.toDataURL('image/png'))
     }
+    setDataURL(canvas.toDataURL("image/png"));
 
     // eslint-disable-next-line
-  }, [updateCount])
+  }, [updateCount]);
 
   useEffect(() => {
-    const { camera, create, animate, controls, destroy } = init(divRef.current)
-    easyThreeRef.current = { camera, create }
+    const { camera, create, animate, controls, destroy } = init(divRef.current);
+    easyThreeRef.current = { camera, create };
     controls.connect();
     controls.enableZoom = false;
     create.ambientLight();
     create.directionalLight();
-    camera.position.set(0, 6, 10)
+    camera.position.set(0, 6, 10);
     const cube = create.cube({ size: 2 });
     create.sky();
     const ocean = create.ocean(dataURL, {
-      textureSize: 128
-    })
-    animate(({ delta, time }) => {
-      cube.rotation.x += delta * 0.8
-      cube.rotation.y += delta
-      cube.position.y = Math.sin(time) * 2 + 3
-      ocean.update(delta)
+      textureSize: 128,
     });
-    setStop(false)
+    animate(({ delta, time }) => {
+      cube.rotation.x += delta * 0.8;
+      cube.rotation.y += delta;
+      cube.position.y = Math.sin(time) * 2 + 3;
+      ocean.update(delta);
+    });
+    setStop(false);
     return () => {
-      destroy()
-    }
-  }, [dataURL])
+      destroy();
+    };
+  }, [dataURL]);
 
   return (
     <>
@@ -126,87 +129,124 @@ function App() {
       </div>
       <div className="flex flex-col justify-center items-center mt-2 gap-4 p-4 pb-20 max-w-xl mx-auto">
         <div>
-          {/* ライセンスを気にせず好きに使ってほしい。もちろん改変も再配布も可能。 */}
-          <p>作成・ダウンロードしたノーマルマップは、個人・商用問わず自由にご利用いただけます。改変や再配布も歓迎です。</p>
-          <a className="text-blue-600" href="https://masabando.github.io/easy-three/">easy-three</a> で使ってもらえると作者は喜びます。
+          <p>
+            水面のノーマルマップを生成します。<br />
+            生成されたノーマルマップは、3Dグラフィックスやゲーム開発において、水面のリアルな凹凸表現に利用できます。
+          </p>
         </div>
         <div className="form-control w-full">
           <label className="label">
-            <span className="label-text">Noise Detail : {noiseDetail}</span>
+            <span className="label-text">
+              Noise Detail(波模様の大きさ) : {noiseDetail}
+            </span>
           </label>
           <input
             disabled={stop}
             ref={noiseDetailRef}
-            type="range" min={10} max={160} defaultValue={noiseDetail}
+            type="range"
+            min={10}
+            max={160}
+            value={noiseDetail}
             className="range range-primary w-full"
             onChange={(e) => {
-              if (noiseDetailTimeout.current) {
-                clearTimeout(noiseDetailTimeout.current)
-              }
-              const value = parseInt(e.target.value, 10)
-              setNoiseDetail(value)
-              noiseDetailTimeout.current = setTimeout(() => {
-                setStop(true)
-                setTimeout(() => {
-                  updateCountDispatch()
-                }, 100)
-                noise.current = new NoiseJS.Noise(Math.random())
-              }, 100)
+              // if (noiseDetailTimeout.current) {
+              //   clearTimeout(noiseDetailTimeout.current);
+              // }
+              const value = parseInt(e.target.value, 10);
+              setNoiseDetail(value);
+              // noiseDetailTimeout.current = setTimeout(() => {
+              //   setStop(true);
+              //   setTimeout(() => {
+              //     updateCountDispatch();
+              //   }, 100);
+              //   noise.current = new NoiseJS.Noise(Math.random());
+              // }, 100);
             }}
           />
         </div>
         <div className="form-control w-full">
           <label className="label">
-            <span className="label-text">NormalMap Strength : {normalMapStrength}</span>
+            <span className="label-text">
+              NormalMap Strength(凹凸の強さ) : {normalMapStrength.toFixed(2)}
+            </span>
           </label>
           <input
             disabled={stop}
             ref={normalMapStrengthRef}
-            type="range" min={0.5} max={10} step={0.02} defaultValue={normalMapStrength}
+            type="range"
+            min={0.5}
+            max={10}
+            step={0.02}
+            value={normalMapStrength}
             className="range range-secondary w-full"
             onChange={(e) => {
-              if (normalMapStrengthTimeout.current) {
-                clearTimeout(normalMapStrengthTimeout.current)
-              }
-              const value = parseFloat(e.target.value)
-              setNormalMapStrength(value)
-              normalMapStrengthTimeout.current = setTimeout(() => {
-                setStop(true)
-                setTimeout(() => {
-                  updateCountDispatch()
-                }, 100)
-              }, 100)
+              // if (normalMapStrengthTimeout.current) {
+              //   clearTimeout(normalMapStrengthTimeout.current);
+              // }
+              const value = parseFloat(e.target.value);
+              setNormalMapStrength(value);
+              // normalMapStrengthTimeout.current = setTimeout(() => {
+              //   setStop(true);
+              //   setTimeout(() => {
+              //     updateCountDispatch();
+              //   }, 100);
+              // }, 100);
             }}
           />
         </div>
+        <div>
+          <button
+            className="btn btn-primary"
+            onClick={() => {
+              setStop(true);
+              updateCountDispatch();
+            }}
+          >
+            ノーマルマップを生成
+          </button>
+        </div>
         <canvas
           ref={ref}
-          width={512} height={512}
+          width={512}
+          height={512}
           className="border max-w-full"
         />
         <div
           ref={divRef}
           style={{
-            width: '200px',
-            aspectRatio: '1',
-            maxWidth: '100%',
-            border: '1px solid #ccc'
-          }}></div>
+            width: "200px",
+            aspectRatio: "1",
+            maxWidth: "100%",
+            border: "1px solid #ccc",
+          }}
+        ></div>
         <button
           className="btn btn-secondary mt-4"
           onClick={() => {
-            const canvas = ref.current
-            const link = document.createElement('a')
-            link.download = 'water-map.png'
-            link.href = canvas.toDataURL('image/png')
-            link.click()
+            const canvas = ref.current;
+            const link = document.createElement("a");
+            link.download = "water-map.png";
+            link.href = canvas.toDataURL("image/png");
+            link.click();
           }}
         >
-          Download Water Map
+          ノーマルマップをダウンロード
         </button>
+        <div>
+          <p>
+            作成・ダウンロードしたノーマルマップは、個人・商用問わず自由にご利用いただけます。改変や再配布も歓迎です。
+          </p>
+          <a
+            className="text-blue-600"
+            href="https://masabando.github.io/easy-three/"
+          >
+            easy-three
+          </a>{" "}
+          で使ってもらえると作者は喜びます。
+        </div>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
